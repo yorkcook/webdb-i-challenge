3a2bdfc941d46548535ada855580cea4ddf5ddc6
York Cook <yorkcook@Johnny-Castle.lan1>
2019-08-13 11:13:09 -0400

    WIP: all CRUD working except post

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require("./server.js");
 const PORT = process.env.PORT || 4002;
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+  console.log(`Reporting for duty on port ${PORT}!`);
 });
--- server.js
+++ server.js
@@ -1,9 +1,68 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Coming to you live from the API!" });
+});
+
+server.get("/accounts", (req, res) => {
+  db("accounts")
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "No, no, no you don't get to see dem accounts." });
+    });
+});
+
+server.post("/accounts", (req, res) => {
+  const post = req.body;
+  db("accounts")
+    .insert(post, "id")
+    .then(account => {
+      res.status(201).json(account);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "You have been....denied!" });
+    });
+});
+
+server.put("/accounts/:id", (req, res) => {
+  const update = req.body;
+  db("accounts")
+    .where("id", "=", req.params.id)
+    .update(update)
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "You done got smurfed!!" });
+    });
+});
+
+server.delete("/accounts/:id", (req, res) => {
+  db("accounts")
+    .where("id", "=", req.params.id)
+    .del()
+    .then(account => {
+      if (account > 0) {
+        res.status(200).json(account);
+      } else {
+        res
+          .status(404)
+          .json({ message: "There ain't no one here with that id!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "You done got smurfed!" });
+    });
+});
 
 module.exports = server;
